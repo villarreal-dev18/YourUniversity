0f75e9fcf609d8049d6dcf39956f00a7249861d5
villarreal-dev18 <metanoiav1812@gmail.com>
2025-04-07 22:24:43 -0500

    feat(modals): implement RegisterModal and enhance LoginModal
    Add RegisterModal component with state management and event handling for opening and closing the modal. Improve LoginModal by fixing a typo in the event listener and adding a fully styled form with input fields, labels, and a submit button. Remove the deprecated login.astro page as its functionality is now handled by the LoginModal.

--- src/components/reacts/modals/LoginModal.jsx
+++ src/components/reacts/modals/LoginModal.jsx
@@ -6,7 +6,7 @@ const LoginModal = () => {
     const [password, setPassword] = useState('');
     useEffect (() => {
         const handleOpenModal = () => setIsOpen(true);
-        window.addEventListener('opne-login-modal', handleOpenModal);
+        window.addEventListener('open-login-modal', handleOpenModal);
         return () => {
             window.removeEventListener('open-login-modal', handleOpenModal);
         };
@@ -18,7 +18,101 @@ const LoginModal = () => {
     };
     if (!isOpen) return null;
     return (
-       <div className=" fixed inset-0 z-50 bg-black opacity-50">
+       <div className=" fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center p-4">
+            <div className="bg-white rounded-2xl shadow-xl max-w-4xl min-h-[600px] h-auto w-full p-32 relative">
+                <button onClick={() =>setIsOpen(false)} className=" absolute top-4 right-4 text-gray-500 hover:text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+                </button>
+                <h1 
+                    className="text-black text-2xl font-bold mb-2 text-center"
+                    style={{fontFamily: "'Inter', sans-serif"}}>
+                    Inicio de Sesión
+                </h1>
+                <div className="flex items-center justify-center mb-10 ">
+                    <p className=" max-w-md text-center  font-medium " style={{fontFamily: "'Inter', sans-serif"}}>
+                        Bienvenido a YourUniversity, ¿Qué hay de nuevo?.
+                        Inicia Sesión para continuar.
+                    </p>
+                </div>
+                <div className="grid py-2 px-10 relative">
+                    <input 
+                    id="email" 
+
+                    className="peer text-black rounded-2xl outline-none border-none
+                    bg-gray-100 px-6 py-4 w-full focus:border-black focus:ring-0
+                    focus:bg-gray-200 placeholder-transparent left-4
+                    placeholder:text-sm hover:cursor-pointer hover:bg-gray-200"
+                    placeholder="Correo"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} required
+                    />
+                    
+                    <label 
+                        htmlFor="email" 
+                        className="absolute left-4 top- text-sm text-gray-600
+                        px-12 transition-all duration-200 ease-out
+                        peer-placeholder-shown:text-base 
+                         peer-placeholder-shown:text-gray-500 
+                        peer-placeholder-shown:top-6 
+                        peer-focus:top-2
+                        peer-focus:text-xs peer-focus:text-black">
+                        Correo
+                    </label>
+                </div>
+                <div class="grid py-2 px-10 relative">
+                    <input 
+                    id="password" 
+                    className="peer text-black rounded-2xl outline-none border-none
+                    bg-gray-100 px-6 py-4 w-full focus:border-black focus:ring-0
+                    focus:bg-gray-200 placeholder-transparent
+                    placeholder:text-sm hover:cursor-pointer hover:bg-gray-200"
+                    placeholder="Contraseña"
+                    type="password" value={password} required/>
+                    <label 
+                        htmlFor="password" 
+                        className="absolute left-4 top- text-sm text-gray-600
+                        px-12 transition-all duration-200 ease-out
+                        peer-placeholder-shown:text-base 
+                         peer-placeholder-shown:text-gray-500 
+                        peer-placeholder-shown:top-6 
+                        peer-focus:top-2
+                        peer-focus:text-xs peer-focus:text-black">
+                        Contraseña
+                    </label>
+                </div>
+                <div className="grid grid-cols-1 items-center mt-14 mx-12 justify-center md:grid-cols-2">
+                
+                    <div className="inline-blockq text-sm">
+                        <input
+                        type="checkbox"
+                        className="ml-2 w-4 h-4 bg-gray-300 border border-gray-300 
+                        focus:ring-0 focus:ring-offset-0 focus:outline-none text-gray-950
+                        focus:ring-transparent rounded-md checked:bg-gray-950 checked:border-transparent"
+                        id="remember-me"/>
+                        <label className="text-gray-600 ml-1" htmlFor="check">Recordarme.
+                        </label>
+                        <div className="text-sm items-center mt-2">
+                            <a href="#" className="font-medium text-neutral-500 hover:text-neutral-900">
+                                ¿Olvidaste tu contraseña?
+                            </a>
+                        </div>
+                    </div>
+                    <div className=" flex justify-end items-center" >
+                        <buttom type="submit" 
+                        className="flex justify-center 
+                        items-center bg-gray-950 rounded-full 
+                        md:py-3 md:px-6 px-2 py-2 text-white font-medium  hover:scale-105"
+                        style={{fontFamily: "'Inter', sans-serif"}}
+                        >
+                            Iniciar Sesión
+                        </buttom>
+                    </div>
+                    
+                </div>
+            </div>
        </div> 
     )
 };
--- src/components/reacts/modals/RegisterModal.jsx
+++ src/components/reacts/modals/RegisterModal.jsx
@@ -1,4 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
+const RegisterModal = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [name, setName] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    useEffect(() => {
+        const handleOpenModal = () => setIsOpen(true);
+        window.addEventListener('open-register-modal', handleOpenModal);
+        
+        return () => {
+          window.removeEventListener('open-register-modal', handleOpenModal);
+        };
+      }, []);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Perform login logic here
+        console.log('Register attempt with:',{ name, lastname, email});
+    };
+    if (!isOpen) return null;
+    return (
+       <div className=" fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center  justify-center p-4">
+            <div className="bg-white rounded-2xl shadow-xl max-w-4xl min-h-[600px] h-auto w-full p-32 relative">
+            <button onClick={() =>setIsOpen(false)} className=" absolute top-4 right-4 text-gray-500 hover:text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+                </button>
+            </div>
+       </div> 
+    )
+};
 
-export default Register;
+export default RegisterModal;
